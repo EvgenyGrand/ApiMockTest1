2cf3b8dd6dd84924f5eba30b8509aeef8c693df2
Arbatron_9000 <Evgeny.Grand@mail.ru>
2025-01-01 22:33:35 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test/java/ApiMockTest.java

--- src/test/java/service/WireMockServerSetup.java
+++ src/test/java/service/WireMockServerSetup.java
@@ -1,6 +1,6 @@
 package service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
